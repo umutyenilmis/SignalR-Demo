6dc78fc72b2aef3ec7d95bad908f5501db9afd93
Umut Yenilmiş <ummittt@gmail.com>
2013-03-28 17:20:39 +0200

    Dosyalar eklendi

---
+++ SignalR.Web/SignalR.Web/SignalrHub/PositionHub.cs
@@ -0,0 +1,25 @@
+﻿using SignalR.Hubs;
+
+
+namespace SignalR.Web.SignalrHub
+{
+    public class PositionHub : Hub
+    {
+        public void Send(string message)
+        {
+            
+        }
+
+        public void ObjectPosition(int x, int y)
+        {
+            var caller = Clients.Caller;
+
+            Clients.setNewPosition(x, y);
+        }
+
+        public void AskCurrentPosition()
+        {
+            Clients.setNewPosition(400, 300);
+        }
+    }
+}
